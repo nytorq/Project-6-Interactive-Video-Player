fdf30ee03f37784285a10c56e49bb919f2f939fc
Charlie Prator <charlieprator@gmail.com>
2018-10-25 22:32:21 -0600

    Finished last requirements
    Added px to em calculator function in Scss, added highlight on hover for
    caption's links, added some styles to mejs controls, added extra JS for
    making captions links to specific points in the video,

--- js/text-highlighter.js
+++ js/text-highlighter.js
@@ -1,19 +1,80 @@
 // Javascript text highlighter
+/*jshint esversion: 6 */
 
+// CONST VARIABLES
+const text = document.querySelectorAll('span');
+const captions = document.getElementsByClassName('captions');
 
+// lET VARIABLES
+let video = document.getElementsByClassName('video');
+let videoTime;
 
-const text = document.querySelectorAll('p');
-const videos = document.querySelectorAll('video');
-const playButton = document.querySelectorAll('button');
-let video = videos[0];
-let currentTime = video.currentTime;
-const videoDuration = video.duration;
+// EVENT LISTENERS
 
-playButton[0].addEventListener('click', () => {
-  let currentTime = video.currentTime;
-  const videoDuration = video.duration;
-  console.log(currentTime+ ' ' + videoDuration);
+video[1].addEventListener('timeupdate', () => {
+  videoTime = video[1].currentTime;
+  currentTime();
 });
+// This puts a listener on the video element tied to time update.
+// When video time updates it updates the videoTime variable and runs
+// the currentTime function.
+
+// FUNCTIONS
+// const currentTime = () => {
+//   let demo = document.getElementById('demo');
+//   demo.innerHTML = videoTime;
+// }
+
+const currentTime = () => {
+  for (let i=0; i<=captions.length; i++) {
+    if (videoTime > captions[i].dataset.start && videoTime < captions[i].dataset.end) {
+      captions[i].classList.add("highlighted");
+    } else {
+      captions[i].classList.remove("highlighted");
+    }
+  }
+};
+
+for (let i=0; i<=captions.length; i++) {
+  captions[i].addEventListener('click', () => {
+    let startTime = captions[i].dataset.start;
+    video[1].currentTime = startTime;
+  });
+}
+
+// This function simply takes the videoTime value and changes the span
+// element below the video with whatever their current time is
+//on the video.
+
+
+/// v2
+//
+//
+// video.on('timeupdate', currentTime);
+//
+// function currentTime() {
+//   let demo = document.getElementById('demo');
+//   demo.innerHTML = videoTime;
+// }
+//
+// for (i=0; i<video.length; i++) {
+//   if (video[i].currentTime > 0) {
+//     const videoTarget = video[i];
+//     return videoTarget
+//   }
+// }
+// const text = document.querySelectorAll('p');
+// const videos = document.querySelectorAll('video');
+// const playButton = document.querySelectorAll('button');
+// let video = videos[0];
+// let currentTime = video.currentTime;
+// const videoDuration = video.duration;
+//
+// playButton[0].addEventListener('click', () => {
+//   let currentTime = video.currentTime;
+//   const videoDuration = video.duration;
+//   console.log(currentTime+ ' ' + videoDuration);
+// });
 
 // while (currentTime != videoDuration) {
 //   currentTime = video.currentTime;
