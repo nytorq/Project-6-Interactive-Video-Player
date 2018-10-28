891d20adba5ee1c2354150793cca9153e5f6434b
Charlie Prator <charlieprator@gmail.com>
2018-10-27 19:40:14 -0600

    Clean up and polishing
    Cleaned up HTML, added some Scss styles to container to make video
    full-width on mobile, cleaned up JS, validated files, checked project in
    Firefox and Edge.

--- js/text-highlighter.js
+++ js/text-highlighter.js
@@ -2,7 +2,6 @@
 /*jshint esversion: 6 */
 
 // CONST VARIABLES
-const text = document.querySelectorAll('span');
 const captions = document.getElementsByClassName('captions');
 
 // lET VARIABLES
@@ -15,16 +14,9 @@ video[1].addEventListener('timeupdate', () => {
   videoTime = video[1].currentTime;
   currentTime();
 });
-// This puts a listener on the video element tied to time update.
-// When video time updates it updates the videoTime variable and runs
-// the currentTime function.
 
-// FUNCTIONS
-// const currentTime = () => {
-//   let demo = document.getElementById('demo');
-//   demo.innerHTML = videoTime;
-// }
 
+// FUNCTIONS AND LOOPS
 const currentTime = () => {
   for (let i=0; i<=captions.length; i++) {
     if (videoTime > captions[i].dataset.start && videoTime < captions[i].dataset.end) {
@@ -41,81 +33,3 @@ for (let i=0; i<=captions.length; i++) {
     video[1].currentTime = startTime;
   });
 }
-
-// This function simply takes the videoTime value and changes the span
-// element below the video with whatever their current time is
-//on the video.
-
-
-/// v2
-//
-//
-// video.on('timeupdate', currentTime);
-//
-// function currentTime() {
-//   let demo = document.getElementById('demo');
-//   demo.innerHTML = videoTime;
-// }
-//
-// for (i=0; i<video.length; i++) {
-//   if (video[i].currentTime > 0) {
-//     const videoTarget = video[i];
-//     return videoTarget
-//   }
-// }
-// const text = document.querySelectorAll('p');
-// const videos = document.querySelectorAll('video');
-// const playButton = document.querySelectorAll('button');
-// let video = videos[0];
-// let currentTime = video.currentTime;
-// const videoDuration = video.duration;
-//
-// playButton[0].addEventListener('click', () => {
-//   let currentTime = video.currentTime;
-//   const videoDuration = video.duration;
-//   console.log(currentTime+ ' ' + videoDuration);
-// });
-
-// while (currentTime != videoDuration) {
-//   currentTime = video.currentTime;
-// }
-
-<!--
-// - Load all <p>s into 'text'
-// - Load <video> into 'video'
-// - Gotta figure out how to continously load new times into currentTime
-// - S
-// 2. Start a timer?
-// 3. Read the .vtt file to see at what time a particular piece of text should be highlighted
-// 4. When this happens, this file should take the string of text tied to the .vtt and look for a match in index.html
-// 5. When a match is found within index.html, that particular string should be wrapped within a <span> with the "highlighted" class tied to it.
-// 6. Repeat steps 3-5
-// 7. When last the last caption's time runs out, the last matching string should have the "highlighted" span tag removed.
--->
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-// PREVIOUS SCRATCHWORK --------------------------------------------------------
-//
-// const textBank = "Now that we've looked at the architecture of the internet, let's see how you might";
-// const matching = '';
-//
-// // const textChanger = (text) => {
-// //   const allParagraphs = document.querySelectorAll('p');
-// //   console.log()
-// // };
-//
-// const allParagraphs = document.querySelectorAll('p');
-// console.log(allParagraphs);
-
-
-// Steps - v1
-// 1. Initiate upon pressing play on Video
-// 2. Start a timer?
-// 3. Read the .vtt file to see at what time a particular piece of text should be highlighted
-// 4. When this happens, this file should take the string of text tied to the .vtt and look for a match in index.html
-// 5. When a match is found within index.html, that particular string should be wrapped within a <span> with the "highlighted" class tied to it.
-// 6. Repeat steps 3-5
-// 7. When last the last caption's time runs out, the last matching string should have the "highlighted" span tag removed.
